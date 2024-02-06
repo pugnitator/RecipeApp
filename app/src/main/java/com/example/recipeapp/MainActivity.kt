e07ad5fd3a538faefef8468eb5643cfd8527b84b
pugnitator <pugnitator2000@gmail.com>
2024-02-06 22:23:14 +0300

    RA-03. Draft commit

--- app/src/main/java/com/example/recipeapp/MainActivity.kt
+++ app/src/main/java/com/example/recipeapp/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.recipeapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.recipeapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var _binding: ActivityMainBinding? = null
+    private val binding
+        get() = _binding ?: throw IllegalAccessException("binding from ActivityMainBinding must not be null")
     override fun onCreate(savedInstanceState: Bundle?) {
+        _binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 }
