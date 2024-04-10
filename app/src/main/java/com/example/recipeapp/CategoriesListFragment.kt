ff740702ee26b1f954fa39f2ab24e33868a62126
pugnitator <pugnitator2000@gmail.com>
2024-04-10 22:42:05 +0300

    RA-04. Add fragment_list_categories

--- app/src/main/java/com/example/recipeapp/CategoriesListFragment.kt
+++ app/src/main/java/com/example/recipeapp/CategoriesListFragment.kt
@@ -4,40 +4,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.recipeapp.databinding.FragmentListCategoriesBinding
 
-//class CategoriesListFragment: Fragment(){
-//
-//    var binding = FragmentCategoriesListBinding? = null
-//    override fun onCreateView(
-//        inflater: LayoutInflater,
-//        container: ViewGroup?,
-//        savedInstanceState: Bundle?
-//    ): View? {
-//        var binding = .inflate(R.layout.fragment_list_categories, container)
-//        return super.onCreateView(inflater, container, savedInstanceState)
-//    }
-//}
-//
+class CategoriesListFragment: Fragment(){
 
-
-class HomeFragment : Fragment() {
-
-    private var _homeBinding: FragmentHomeBinding? = null
-    private val homeBinding get() = _homeBinding!!
+    private var _binding : FragmentListCategoriesBinding? = null
+    private val binding
+        get() = _binding
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
-
-        _homeBinding = FragmentHomeBinding.inflate(inflater,container,false)
-        val view  = homeBinding.root
+    ): View? {
+        _binding = FragmentListCategoriesBinding.inflate(layoutInflater)
+        val view = binding?.root
         return view
     }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _homeBinding = null
-    }
-
 }
--- app/src/main/java/com/example/recipeapp/MainActivity.kt
+++ app/src/main/java/com/example/recipeapp/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.recipeapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.add
+import androidx.fragment.app.commit
 import com.example.recipeapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -14,5 +16,11 @@ class MainActivity : AppCompatActivity() {
         _binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        if (savedInstanceState == null) {
+            supportFragmentManager.commit {
+                setReorderingAllowed(true)
+                add<CategoriesListFragment>(R.id.mainContainer)
+            }
+        }
     }
 }
