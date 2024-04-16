51bf59ea62862373521024edac5a7a0d432b74c2
pugnitator <pugnitator2000@gmail.com>
2024-04-16 15:21:56 +0300

    RA-07. Local commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,4 +50,5 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation("androidx.fragment:fragment-ktx:$fragment_version")
+    implementation("androidx.cardview:cardview:1.0.0")
 }
