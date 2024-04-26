2d5e1583f7901c132c29a6da6f65fafeb551a3bd
pugnitator <pugnitator2000@gmail.com>
2024-04-26 17:00:52 +0300

    RA-08. Add CategoriesListAdapter, implement RV

--- app/src/main/java/com/example/recipeapp/CategoriesListAdapter.kt
+++ app/src/main/java/com/example/recipeapp/CategoriesListAdapter.kt
@@ -1,35 +1,54 @@
 package com.example.recipeapp
 
-import android.content.Context
+import android.graphics.drawable.Drawable.createFromStream
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.core.graphics.drawable.toDrawable
 import androidx.recyclerview.widget.RecyclerView
 import com.example.recipeapp.databinding.ItemCategoryBinding
+import java.io.FileNotFoundException
+import java.io.InputStream
 
 class CategoriesListAdapter(
-    private val dataSet: Set<Category>
+    private val dataSet: List<Category>
 ) : RecyclerView.Adapter<CategoriesListAdapter.ViewHolder>() {
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         private val binding = ItemCategoryBinding.bind(itemView)
 
-        fun bind(category: Category) {
-            with(binding) {
-                ivCategoryItem.setImageResource()
-                tvCategoryItemTitle.text = category.title
-                tvCategoryItemDescription.text = category.description
-            }
-        }
+        val ivCategory: ImageView = binding.ivCategoryItem
+        val tvTitle: TextView = binding.tvCategoryItemTitle
+        val tvDescription: TextView = binding.tvCategoryItemDescription
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.item_category, parent, false)
+        val itemView =
+            LayoutInflater.from(parent.context).inflate(R.layout.item_category, parent, false)
         return ViewHolder(itemView)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.bind(STUB.getCategories()[position])
+        val category = dataSet[position]
+
+        val image = try {
+            val inputStream: InputStream? =
+                holder.ivCategory.context?.assets?.open(category.imageUrl)
+            createFromStream(inputStream, category.imageUrl)
+        } catch (e: FileNotFoundException) {
+            val tag = "CategoryListAdapter"
+            val errorLog = Log.e(tag, "Failed to retrieve file from assets")
+            R.drawable.stub.toDrawable()
+        }
+
+        with(holder) {
+            ivCategory.setImageDrawable(image)
+            tvTitle.text = category.title
+            tvDescription.text = category.description
+        }
     }
 
     override fun getItemCount(): Int {
